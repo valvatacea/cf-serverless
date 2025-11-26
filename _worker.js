d7170de35304dee213c1b9b299f41a4dc48b3dab
Patrick SmartPants <13595237+valvatacea@users.noreply.github.com>
2025-11-26 17:24:08 +0700

    update _worker.js

--- _worker.js
+++ _worker.js
@@ -40,7 +40,7 @@ const RELAY_SERVER_UDP = {
   host: "udp-relay.hobihaus.space", // Kontribusi atau cek relay publik disini: https://hub.docker.com/r/kelvinzer0/udp-relay
   port: 7300,
 };
-const PRX_HEALTH_CHECK_API = "https://id1.foolvpn.me/api/v1/check";
+const PRX_HEALTH_CHECK_API = "https://cf-serverless.valvatacea.workers.dev/api/v1/check";
 const CONVERTER_URL = "https://api.foolvpn.me/convert";
 const WS_READY_STATE_OPEN = 1;
 const WS_READY_STATE_CLOSING = 2;
